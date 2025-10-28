5e6f5d511ec01a47ccea305aec46122df49c10f7
Mikhail <akuloff.m@gmail.com>
2025-10-28 15:55:02 +0300

    updated KafkaProducerConfig & KafkaProducerService used JsonSerializer

--- src/main/java/com/akulov/springboot/userservice_withkafka/configuration/KafkaProducerConfig.java
+++ src/main/java/com/akulov/springboot/userservice_withkafka/configuration/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package com.akulov.springboot.userservice_withkafka.configuration;
 
+import com.akulov.springboot.userservice_withkafka.dto.NotificationDto;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,16 +21,19 @@ public class KafkaProducerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, NotificationDto> producerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        config.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+
         return new DefaultKafkaProducerFactory<>(config);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, NotificationDto> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
--- src/main/java/com/akulov/springboot/userservice_withkafka/service/KafkaProducerService.java
+++ src/main/java/com/akulov/springboot/userservice_withkafka/service/KafkaProducerService.java
@@ -2,7 +2,6 @@ package com.akulov.springboot.userservice_withkafka.service;
 
 import com.akulov.springboot.userservice_withkafka.dto.NotificationDto;
 import com.akulov.springboot.userservice_withkafka.enums.OperationType;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,24 +12,21 @@ import org.springframework.stereotype.Service;
 public class KafkaProducerService {
     private static final Logger log = LoggerFactory.getLogger(KafkaProducerService.class);
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, NotificationDto> kafkaTemplate;
 
     @Value("${app.kafka.topic.user-notifications:user-notifications}")
     private String userNotificationsTopic;
 
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+    public KafkaProducerService(KafkaTemplate<String, NotificationDto> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
-        this.objectMapper = objectMapper;
     }
 
     public void sendNotification(OperationType operationType, String email, String userName) {
         log.info("KafkaProducerService. Attempting to send notification about operation to email");
         try {
             NotificationDto notification = new NotificationDto(operationType, email, userName);
-            String message = objectMapper.writeValueAsString(notification);
 
-            kafkaTemplate.send(userNotificationsTopic, message);
+            kafkaTemplate.send(userNotificationsTopic, notification);
             log.info("Notification about operation: {} was successfully sent to email: {}", operationType, email);
         } catch (Exception e) {
             log.error("Failed to send notification about operation: {} to email: {}",  operationType, email ,e);
